273aff18f70076e330b214054e8a3933abbe6cb1
cristianPMS <luiswarobots777@gmail.com>
2025-03-12 12:16:39 -0600

    actualizar_db

--- models/database.js
+++ models/database.js
@@ -4,10 +4,10 @@ console.log('Intentando conectar a la base de datos...');
 
 // Crear la conexión a la base de datos
 const connect = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Usuario de tu base de datos
-    password: '', // Contraseña de tu base de datos
-    database: 'syatec' // Nombre de tu base de datos
+    host: 'bg2rxbcqqcnyb4vvvu3q-mysql.services.clever-cloud.com',
+    user: 'utngeehqzhfz5mfb', // Usuario de tu base de datos
+    password: 'kVl7c21Jct07HTMNMMoh', // Contraseña de tu base de datos
+    database: 'bg2rxbcqqcnyb4vvvu3q' // Nombre de tu base de datos
 });
 
 // Verificar la conexión
